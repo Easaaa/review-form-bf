f43a2c9f2144756fc9a50874c916bfb59d480264
Leonardo Tononi <easaaa@protonmail.com>
2019-05-03 15:02:09 +0200

    init heroku

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ mongoose.connect('mongodb://127.0.0.1/test', {
   useNewUrlParser: true
 });
 
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
